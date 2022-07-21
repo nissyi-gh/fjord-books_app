5696b3537b7be1252ac6579bcba38bb689928afd
nissyi <yuta.onishi@pepabo.com>
2022-07-21 16:46:03 +0900

    MiniMagickを導入し、圧縮した画像を使用できるようにする
    ユーザー一覧、編集画面などでプロフィール画像を表示できるようにします。

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_one_attached :avatar
+
+  def avatar_thumbnail
+    avatar.variant(gravity: :center, resize:"48x48^", crop:"48x48+0+0")
+  end
 end
