cd49708953a63b5016d498d83fa6672ab4bd0e37
nissyi <yuta.onishi@pepabo.com>
2022-07-22 11:50:55 +0900

    follow_relationshipsのmigrationファイルを削除
    モデルを作り忘れていたので一旦消去します。

--- db/migrate/20220722023409_create_follow_relationships.rb
+++
@@ -1,9 +0,0 @@
-class CreateFollowRelationships < ActiveRecord::Migration[6.1]
-  def change
-    create_table :follow_relationships do |t|
-      t.integer :follow_id, foreign_key: true
-      t.integer :follower_id, foreign_key: true
-      t.timestamps
-    end
-  end
-end
