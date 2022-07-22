6fd99f6848f793dbbe4e6598cc1e3dab12c0f503
nissyi <yuta.onishi@pepabo.com>
2022-07-22 11:41:38 +0900

    follow_relationshipsテーブルを作成
    フォロー・フォロワー関係を保存できるようにします。

---
+++ db/migrate/20220722023409_create_follow_relationships.rb
@@ -0,0 +1,9 @@
+class CreateFollowRelationships < ActiveRecord::Migration[6.1]
+  def change
+    create_table :follow_relationships do |t|
+      t.integer :follow_id, foreign_key: true
+      t.integer :follower_id, foreign_key: true
+      t.timestamps
+    end
+  end
+end
