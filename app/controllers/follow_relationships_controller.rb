d1a9386440acce6af59214dc11fb2dd505c3e2ac
nissyi <yuta.onishi@pepabo.com>
2022-07-25 11:21:38 +0900

    画面上からユーザーをフォロー・フォロー解除できるようにする
    ユーザー詳細画面から実行できるようにします。

---
+++ app/controllers/follow_relationships_controller.rb
@@ -0,0 +1,18 @@
+class FollowRelationshipsController < ApplicationController
+  def create
+    current_user.following_relationships.create(follow_id: params[:target_id])
+    find_user_and_redirect
+  end
+  
+  def destroy
+    current_user.following_relationships.find_by(follow_id: params[:target_id]).destroy
+    find_user_and_redirect
+  end
+
+  private
+
+  def find_user_and_redirect
+    @user = User.find(params[:target_id])
+    redirect_to @user
+  end   
+end
