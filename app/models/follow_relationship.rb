08a49c04325d9f2f7fc6e76a0d249bc013735c1b
nissyi <yuta.onishi@pepabo.com>
2022-08-01 12:50:13 +0900

    FollowRelationshipでfollow_idとfollower_idが重複しないようにする
    user_id: 1がuser_id: 1をフォロー、などできないようにします。

--- app/models/follow_relationship.rb
+++ app/models/follow_relationship.rb
@@ -3,6 +3,6 @@
 class FollowRelationship < ApplicationRecord
   belongs_to :follow, class_name: 'User'
   belongs_to :follower, class_name: 'User'
-  validates :follow_id, presence: true
-  validates :follower_id, presence: true
+  validates :follow_id, presence: true, numericality: { other_than: :follower_id }
+  validates :follower_id, presence: true, numericality: { other_than: :follow_id }
 end
