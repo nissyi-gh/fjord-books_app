a49dbe6969cf7a0043a60f1d5a16828e9bb6ecbb
nissyi <yuta.onishi@pepabo.com>
2022-08-01 11:24:03 +0900

    FollowRelationshipに存在性の検証を追加
    follow_id, follower_idが入力されていない場合は無効にします。

--- app/models/follow_relationship.rb
+++ app/models/follow_relationship.rb
@@ -3,4 +3,6 @@
 class FollowRelationship < ApplicationRecord
   belongs_to :follow, class_name: 'User'
   belongs_to :follower, class_name: 'User'
+  validates :follow_id, presence: true
+  validates :follower_id, presence: true
 end
