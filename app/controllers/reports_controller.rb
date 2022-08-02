c547fb0a8fcb72290f72a4641a43f73fdeff1e32
nissyi <yuta.onishi@pepabo.com>
2022-08-02 11:38:26 +0900

    日報を日報を作成できるようにする
    新規を作成したら個別ページに飛びます。個別ページの内容は未作成です。

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -2,4 +2,27 @@ class ReportsController < ApplicationController
   def index
     @reports = Report.order(:id).page(params[:page])
   end
+
+  def new
+    @report = Report.new
+  end
+
+  def show
+  end
+
+  def create
+    @report = Report.new(report_params)
+
+    if @report.save
+      redirect_to @report, notice: t('controllers.common.notice_create', name: Report.model_name.human)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def report_params
+    params.require(:report).permit(:title, :body)
+  end
 end
