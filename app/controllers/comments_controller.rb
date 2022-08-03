0194a8394fd6e1490b755a5405b08fe29c543f60
nissyi <yuta.onishi@pepabo.com>
2022-08-03 10:50:33 +0900

    rubocop対応
    各修正にあたって発生したrubocopへの対応を行います。

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,18 +6,18 @@ class CommentsController < ApplicationController
   def create
     @comment = @target.comments.new(body: params[:body], user_id: current_user.id)
 
-    if @comment.save
-      redirect_to @target, notice: t('controllers.common.notice_create', name: Comment.model_name.human)
-    end
+    return unless @comment.save
+
+    redirect_to @target, notice: t('controllers.common.notice_create', name: Comment.model_name.human)
   end
 
   def destroy
     comment = Comment.find(params[:id])
 
-    if current_user?(comment.user)
-      comment.destroy
-      redirect_to @target, notice: t('controllers.common.notice_destroy', name: Comment.model_name.human)
-    end
+    return unless current_user?(comment.user)
+
+    comment.destroy
+    redirect_to @target, notice: t('controllers.common.notice_destroy', name: Comment.model_name.human)
   end
 
   private
