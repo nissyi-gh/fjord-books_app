1359fb8bd9ba41c3dfdb524d58140d34ac277e5f
nissyi <yuta.onishi@pepabo.com>
2022-08-10 16:37:02 +0900

    not_null制約をつけるmigrationを作成
    not_nullであるべき箇所に制約をかけます。

---
+++ db/migrate/20220810073114_add_not_null_to_comments.rb
@@ -0,0 +1,8 @@
+class AddNotNullToComments < ActiveRecord::Migration[6.1]
+  def change
+    change_column_null :comments, :body, false
+    change_column_null :comments, :user_id, false
+    change_column_null :comments, :commentable_type, false
+    change_column_null :comments, :commentable_id, false
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_02_074534) do
+ActiveRecord::Schema.define(version: 2022_08_10_073455) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -50,10 +50,10 @@ ActiveRecord::Schema.define(version: 2022_08_02_074534) do
   end
 
   create_table "comments", force: :cascade do |t|
-    t.text "body"
-    t.integer "user_id"
-    t.string "commentable_type"
-    t.integer "commentable_id"
+    t.text "body", null: false
+    t.integer "user_id", null: false
+    t.string "commentable_type", null: false
+    t.integer "commentable_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["commentable_type", "commentable_id"], name: "index_comments_on_commentable"
@@ -70,11 +70,11 @@ ActiveRecord::Schema.define(version: 2022_08_02_074534) do
   end
 
   create_table "reports", force: :cascade do |t|
-    t.string "title"
-    t.text "body"
+    t.string "title", null: false
+    t.text "body", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "user_id"
+    t.integer "user_id", null: false
     t.index ["user_id"], name: "index_reports_on_user_id"
   end
 
