aaf784701040b4a2feceeab3b7c53e8f651f75fa
nissyi <yuta.onishi@pepabo.com>
2022-08-10 15:41:41 +0900

    日報のコメントに関する機能をreport/comments_controllerに移動
    ポリモーフィックに対応するため移動します。

--- app/controllers/report/comments_controller.rb
+++ app/controllers/report/comments_controller.rb
@@ -1,2 +1,26 @@
 class Report::CommentsController < ApplicationController
+  before_action :set_report
+
+  def create
+    @comment = @report.comments.new(body: params[:body], user_id: current_user.id)
+
+    if @comment.save
+      redirect_to @report, notice: t('controllers.common.notice_create', name: Comment.model_name.human)
+    else
+      @comments = @report.comments.eager_load(:user)
+      render 'reports/show'
+    end
+  end
+
+  def destroy
+    current_user.comments.find(params[:id]).destroy
+
+    redirect_to @report, notice: t('controllers.common.notice_destroy', name: Comment.model_name.human)
+  end
+
+  private
+
+  def set_report
+    @report = Report.find(params[:report_id])
+  end
 end
