43d72eb9c178e1b8695dba710b935528786e5ece
nissyi <yuta.onishi@pepabo.com>
2022-08-10 15:45:22 +0900

    BookのCommentsに関する操作をbook/comments_controllerに移動
    ポリモーフィックに対応するため、一つのコントローラーでまとめて操作しないようにします。

--- app/controllers/book/comments_controller.rb
+++ app/controllers/book/comments_controller.rb
@@ -1,2 +1,26 @@
 class Book::CommentsController < ApplicationController
+  before_action :set_book
+
+  def create
+    @comment = @book.comments.new(body: params[:body], user_id: current_user.id)
+
+    if @comment.save
+      redirect_to @book, notice: t('controllers.common.notice_create', name: Comment.model_name.human)
+    else
+      @comments = @book.comments.eager_load(:user)
+      render 'books/show'
+    end
+  end
+
+  def destroy
+    current_user.comments.find(params[:id]).destroy
+
+    redirect_to @book, notice: t('controllers.common.notice_destroy', name: Comment.model_name.human)
+  end
+
+  private
+
+  def set_book
+    @book = Book.find(params[:book_id])
+  end
 end
